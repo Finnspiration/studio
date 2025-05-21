0bf553e2cca49ff6761b459221de501ecb1d6507
Finn Kollerup <finn@finnkollerup.com>
2025-05-21 19:59:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppHeader>
    
    with the text content:
    
        Synapse Scribble
    
    Relevant files:
    
        - https://6000-firebase-studio-1747745156341.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1747745156341.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1747745156341.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Teksten her skal være 'FraimeWorks Lite'

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -5,7 +5,7 @@ export function AppHeader() {
     <header className="p-4 border-b border-border">
       <div className="container mx-auto flex items-center gap-2">
         <BrainCircuit className="h-8 w-8 text-primary" />
-        <h1 className="text-2xl font-semibold text-foreground">Synapse Scribble</h1>
+        <h1 className="text-2xl font-semibold text-foreground">FraimeWorks Lite</h1>
       </div>
     </header>
   );
